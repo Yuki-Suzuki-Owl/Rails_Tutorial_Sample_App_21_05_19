f2791e85a63c115702828e3ab492fa0351e17536
root <root@localhost.localdomain>
2021-06-08 10:53:06 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # get 'users/new'
   root 'static_pages#home'
   # get 'static_pages/home'
   # get 'static_pages/help'
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   get '/about'  ,to:'static_pages#about'
   get '/contact',to:'static_pages#contact'
   get '/signup' ,to:'users#new'
+  post '/signup',to:'users#create'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
